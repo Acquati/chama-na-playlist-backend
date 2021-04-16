ab639bdfb4f2fd9f1979759a2c6c91f503434579
Leandro Acquati <leandroacquati@gmail.com>
2021-04-16 03:45:05 -0300

    update jwt check

--- src/middlewares/checkJwt.ts
+++ src/middlewares/checkJwt.ts
@@ -1,25 +1,31 @@
 import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
+import { IJwtPayload } from './IJwtPayload'
 
 export const checkJwt = (request: Request, response: Response, next: NextFunction) => {
-  const token = <string>request.headers['token']
-  let jwtPayload: any
+  const token = request.headers['x-access-token']
+
+  if (!token) {
+    return response.status(401).json({ message: 'No token provided.' })
+  }
+
+  let jwtPayload: string | object
 
   try {
-    jwtPayload = jwt.verify(token, process.env.JWT_SECRET)
+    jwtPayload = jwt.verify(token as string, process.env.JWT_SECRET)
     response.locals.jwtPayload = jwtPayload
   } catch (error) {
     return response.status(401).json({ message: 'Unauthorized access.' })
   }
 
-  const { id, email } = jwtPayload
+  const { id, email } = jwtPayload as IJwtPayload
   const newToken = jwt.sign(
     { id, email },
     process.env.JWT_SECRET,
     { expiresIn: '16h' }
   )
 
-  response.setHeader('token', newToken)
+  response.setHeader('x-access-token', newToken)
 
   next()
 }
